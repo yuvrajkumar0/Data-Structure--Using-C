6f2d2ebbd4331528b0487cab985bc7f05d263208
yuvrajkumar0 <yuvrajkunarmuz745@gmail.com>
2026-07-26 22:38:07 +0530

    challenge no.7

---
+++ seven.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int a[5] = {10, 20, 30, 40, 50};
+    int n = 5, pos;
+
+    printf("Original array: ");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    printf("\nEnter position to delete: ");
+    scanf("%d", &pos);
+
+    if (pos < 1 || pos > n) {
+        printf("Invalid position");
+    } 
+    else {
+        for (int i = pos - 1; i < n - 1; i++) {
+            a[i] = a[i + 1];
+        }
+
+        n--;
+
+        printf("Array after deletion: ");
+
+        for (int i = 0; i < n; i++) {
+            printf("%d ", a[i]);
+        }
+    }
+
+    return 0;
+}
